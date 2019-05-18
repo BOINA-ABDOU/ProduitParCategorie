456733e65efc4973f614390384b749b0f6132422
HOUSNIYA <HOUSNIYA@HOUSNIYA>
2019-05-18 20:26:21 +0200

    commit 1

---
+++ ProduitParCategorie/src/main/java/org/sid/miage/ProduitParCategorieApplication.java
@@ -0,0 +1,14 @@
+package org.sid.miage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProduitParCategorieApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProduitParCategorieApplication.class, args);
+		System.out.println("Bienvenue dans l'application Web.");
+	}
+
+}
